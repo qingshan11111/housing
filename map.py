343ffef61cf8f72048d10a7f650f47b51cb2760a
qingshan11111 <18011399837@163.com>
2024-09-24 20:17:55 +0800

    Add files via upload

--- map.py
+++ map.py
@@ -4,36 +4,33 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 sns.set()
 
-# 设置标题
-st.title('California Housing Data (1990)')
-
-# 读取数据
+st.title('California Housing Data(1990)')
 df = pd.read_csv('housing.csv')
 
-# 过滤器：最低房价
 price_filter = st.slider('Minimal median Housing Price', 0, 500001, 200000)
-
-# 过滤器：位置类型
 location_filter = st.sidebar.multiselect(
     'Choose the location type',
-    df['ocean_proximity'].unique(),  # 选项
-    df['ocean_proximity'].unique())  # 默认值为所有选项
+    df['ocean_proximity'].unique(),  # options
+    df['ocean_proximity'].unique()) 
 
-# 过滤器：收入水平
-income_level = st.radio("Select Income level:", ('Low', 'Medium', 'High'))
+income_level = st.radio("select Income level:",('Low','Medium','High'))
 
-# 根据收入水平过滤
-if income_level == 'Low':
-    filtered_df = df[df['median_income'] <= 2.5]
-elif income_level == 'Medium':
-    filtered_df = df[(df['median_income'] > 2.5) & (df['median_income'] < 4.5)]
+if income_level == 'Low(<=2.5)':
+    filtered_df=df[df['median income']<= 2.5]
+elif income_level =='Medium(> 2.5 & < 4.5)':
+    filtered_df = df[(df['median_income']>2.5)& (df['median_income'] < 4.5)]
 else:
-    filtered_df = df[df['median_income'] >= 4.5]
+    filtered_df = df[df['median income'] > 4.5]
 
-# 根据房价和位置过滤数据
-df = df[df['median_house_value'] >= price_filter]
-df = df[df['ocean_proximity'].isin(location_filter)]
+df = df[df.median_house_value >= price_filter]
+df = df[df.ocean_proximity.isin(location_filter)]
 
-# 显示地图
+if income_level == 'Low(<=2.5)':
+    filtered_df = df[df('median_income')<= 2.5]
+elif income_level == 'Medium(>2.5 & < 4.5)':
+    filtered_df = df[(df('medium_income')>2.5) & (df('medium_income')<4.5) ]
+else:
+    filtered_df = df[df('median_income')>4.5]
+# show on map
 st.map(df)
 
